bf5f3e6ad92a35aa13ef8f386d3d8ef9deb892b5
Stefano Antonelli <stefano.antonelli.2024@gmail.com>
2025-02-25 20:26:54 +0100

    feat: fixed main and methods

--- j17-coding-exercises-1/src/main/java/com/example/StreamProcessRunner.java
+++ j17-coding-exercises-1/src/main/java/com/example/StreamProcessRunner.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Map;
 
-// main
+// main class
 public class StreamProcessRunner {
     private static final Logger logger = LoggerFactory.getLogger(StreamProcessRunner.class);
 
